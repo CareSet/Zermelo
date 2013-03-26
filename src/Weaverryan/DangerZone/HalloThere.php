7813addf41dec712e29203e417dcdd78d4ba860d
Ryan Weaver <ryan@thatsquality.com>
2013-03-26 18:08:23 -0500

    Bootstrapping a single namespaced file

---
+++ src/Weaverryan/DangerZone/HalloThere.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Weaverryan\DangerZone;
+
+/**
+ * A terribly useful class
+ *
+ * See: http://knpuniversity.com/screencast/question-answer-day
+ */
+class HalloThere
+{
+    public function sayHallo()
+    {
+        return 'Hallo!';
+    }
+}
