2a22dd183321429a08be3e0d1cb44b0fb2a74941
Phu Nguyen <phu@ip-172-31-88-249.ec2.internal>
2017-12-10 14:52:40 -0600

    removed jwt auth server

--- src/CareSet/CareSetJWTAuthClient/Model/User.php
+++ src/CareSet/CareSetJWTAuthClient/Model/User.php
@@ -7,84 +7,22 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable
 {
     use Notifiable;
 
 
-    protected static $admin_domains = ['careset.com'];
-    protected $table = 'user';
+    protected $table = 'auth.user';
 
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name','email','is_admin','last_token'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
-        'password', 'remember_token', 'pivot','verification_token'
+        'password', 'remember_token','last_token'
     ];
 
-    /**
-     * Automatically creates hash for the user password.
-     *
-     * @param  string  $value
-     * @return void
-     */
-    public function setPasswordAttribute($value)
-    {
-        $this->attributes['password'] = Hash::make($value);
-    }
 
-    /**
-     * Get the identifier that will be stored in the subject claim of the JWT.
-     *
-     * @return mixed
-     */
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
-    public function getJWTCustomClaims()
-    {
-        return [
-            'name' => $this->name,
-            'email' => $this->email,
-            'is_activated'=>$this->is_activated,
-            'is_admin' => $this->IsAdmin(),
-            'apps' => $this->Apps
-        ];
-    }
-
-    public function IsAdmin()
-    {
-        return self::IsAdminEmail($this->email);
-    }
-
-    public function Apps()
-    {
-        return $this->belongsToMany('App\App','user_app');
-    }
-
-    public static function IsAdminEmail($email)
-    {
-        $split = explode("@",$email);
-        return in_array($split[1],self::$admin_domains);
-    }
 
 }
+
