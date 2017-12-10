69d13ab42fb19261ee42a95834b15ae47a6f1a18
Phu Nguyen <phu@ip-172-31-88-249.ec2.internal>
2017-12-10 14:38:46 -0600

    added full MVC

---
+++ src/CareSet/CareSetJWTAuthClient/Controller/CareSetJWTLoginController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace CareSet\CareSetJWTAuthClient\Controller;
+
+use Illuminate\Http\Request;
+
+use \Firebase\JWT\JWT;
+use \Exception;
+use \UnexpectedValueException;
+use \Firebase\JWT\BeforeValidException;
+use \Firebase\JWT\SignatureInvalidException;
+use \Firebase\JWT\ExpiredException;
+use Illuminate\Support\Facades\Auth;
+use \CareSet\CareSetJWTAuthClient\Model\User;
+
+class CareSetJWTLoginController extends Controller
+{
+    public function callback(Request $request)
+    {
+    	$callback = $request->callback;
+    	$return = $request->return;
+    	$token = $request->token;
+
+    	$user = JWT::decode($token,config('caresetjwtclient.public_key'),config('caresetjwtclient.algo'));
+
+    	$User = User::where('email',$user->email)->first();
+
+    	if(!$User)
+    	{
+			$User = User::Create([
+		        	'name'=>$user->name,
+		        	'email'=>$user->email,
+		        	'last_token'=>$token,
+		        	'is_admin'=>$user->is_admin
+		        ]);
+		} else
+		{
+			$User->update([
+		        	'name'=>$user->name,
+		        	'email'=>$user->email,
+		        	'last_token'=>$token,
+		        	'is_admin'=>$user->is_admin
+		        ]);
+			$User->save();
+			
+		}
+
+        auth::login($User);
+
+        return redirect($return);
+    }
+
+    public function logout(Request $request)
+    {
+    	auth::logout();
+
+
+    	return redirect('/');
+    }
+}
--- src/CareSet/CareSetJWTAuthClient/config/caresetjwtclient.php
+++ src/CareSet/CareSetJWTAuthClient/config/caresetjwtclient.php
@@ -5,7 +5,19 @@
  */
 
 return [
+
+	"a"=>"asdfaf",
+	
+	'applicaiton_token'=>env("CARESET_TOKEN",""),
+
+	'auth_login'=>env("CARESET_JWT_LOGIN","https://auth.careset.com/auth/cs/login"),
+
+	'callback_url'=>env("CARESET_JWT_CALLBACK",url('/auth/cs/callback')),
+
+	'return_url'=>env("CARESET_JWT_RETURN",url('/')),
+
     'public_key' => file_get_contents(base_path('keys/jwt_public_key.pub')),
+
     'algo' => 'HS256'
 
 ];
