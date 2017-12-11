34cd38442bf9720d37420b904c82cd3eae74b05b
Phu Nguyen <phu@ip-172-31-88-249.ec2.internal>
2017-12-10 19:17:13 -0600

    removed the auth config bootstrap

--- src/CareSet/CareSetJWTAuthClient/ServiceProvider.php
+++ src/CareSet/CareSetJWTAuthClient/ServiceProvider.php
@@ -9,9 +9,7 @@ Class ServiceProvider extends \Illuminate\Support\ServiceProvider
 
 	public function register()
 	{
-	    $this->mergeConfigFrom(
-	        __DIR__.'/config/auth.php', 'auth'
-	    );
+
 	}
 
 	public function boot(\Illuminate\Routing\Router $router)
