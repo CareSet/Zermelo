394a91a51f101119325506385ddc5a0dc732c464
Phu Nguyen <phu@ip-172-31-88-249.ec2.internal>
2017-12-10 18:10:12 -0600

    i think i solved the problem

--- src/CareSet/CareSetJWTAuthClient/ServiceProvider.php
+++ src/CareSet/CareSetJWTAuthClient/ServiceProvider.php
@@ -21,15 +21,15 @@ Class ServiceProvider extends \Illuminate\Support\ServiceProvider
 
 		 $this->loadRoutesFrom(__DIR__.'/routes.php');
 
-		$router->middleware('careset', \CareSet\CareSetJWTAuthClient\Middleware\JWTClientMiddleware::class);
+		//$router->middleware('careset', \CareSet\CareSetJWTAuthClient\Middleware\JWTClientMiddleware::class);
 
 		Storage::MakeDirectory(base_path('keys'));
-		Storage::delete(base_path('app/User.php'));
+		//Storage::delete(base_path('app/User.php'));
 
 	    $this->publishes([
 	        __DIR__.'/config/caresetjwtclient.php' => config_path('caresetjwtclient.php'),
 	        __DIR__.'/keys/jwt_public_key.pub' => base_path('keys/jwt_public_key.pub'),
-	        __DIR__.'/Model/User.php' => base_path('app/User.php'),
+	       // __DIR__.'/Model/User.php' => base_path('app/User.php'),
 	    ]);
 	}
 
--- src/CareSet/CareSetJWTAuthClient/routes.php
+++ src/CareSet/CareSetJWTAuthClient/routes.php
@@ -1,8 +1,12 @@
 <?php
 
 
-Route::get('/auth/cs/callback','\CareSet\CareSetJWTAuthClient\Controller\CareSetJWTLoginController@callback');
-Route::get('/logout','\CareSet\CareSetJWTAuthClient\Controller\CareSetJWTLoginController@logout');    
+Route::middleware('web')->group(function() {
+	
+	Route::get('/auth/cs/callback','\CareSet\CareSetJWTAuthClient\Controller\CareSetJWTLoginController@callback');
+	Route::get('/logout','\CareSet\CareSetJWTAuthClient\Controller\CareSetJWTLoginController@logout');    
+
+});
 
 Route::middleware(['careset'])->group(function () {
 
