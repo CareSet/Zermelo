ee74c468c7d10887ece2f99efa7caed790fd763d
Phu Nguyen <phu@ip-172-31-88-249.ec2.internal>
2017-12-12 12:04:33 -0600

    add auth logout

--- src/CareSet/CareSetJWTAuthClient/config/caresetjwtclient.php
+++ src/CareSet/CareSetJWTAuthClient/config/caresetjwtclient.php
@@ -9,6 +9,8 @@ return [
 	'applicaiton_token'=>env("CARESET_TOKEN",""),
 
 	'auth_login'=>env("CARESET_JWT_LOGIN","https://auth.careset.com/auth/cs/login"),
+	
+	'auth_logout'=>env("CARESET_JWT_LOGOUT","https://auth.careset.com/auth/cs/logout"),
 
 	'callback_url'=>env("CARESET_JWT_CALLBACK",'https://localhost/auth/cs/callback'),
 
