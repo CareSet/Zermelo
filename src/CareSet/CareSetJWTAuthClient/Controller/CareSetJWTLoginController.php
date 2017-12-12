5ac9fb96a661f476abd2e3bda2815297bcbac678
Phu Nguyen <phu@ip-172-31-88-249.ec2.internal>
2017-12-12 10:44:23 -0600

    added logout

--- src/CareSet/CareSetJWTAuthClient/Controller/CareSetJWTLoginController.php
+++ src/CareSet/CareSetJWTAuthClient/Controller/CareSetJWTLoginController.php
@@ -53,9 +53,44 @@ class CareSetJWTLoginController extends Controller
 
     public function logout(Request $request)
     {
-    	auth::logout();
+
+        if (Auth::guard()->check()) {
+    		
+    		auth::logout();
 
 
-    	return redirect('/');
+            $url = config('caresetjwtclient.auth_logout');
+            $token = config('caresetjwtclient.applicaiton_token');
+            $callback  = config('caresetjwtclient.callback_url');
+            $return = config('caresetjwtclient.return_url');
+
+            $parts = parse_url($url);
+
+            $parameters = [];
+            if (isset($parts['query'])) {
+                $parameters = self::http_parse_query($parts['query']);
+            }
+            $parameters['token'] = $token;
+            $parameters['callback'] = $callback;
+            $parameters['return'] = $request->url();
+
+            $parts['query'] = http_build_query($parameters);
+
+            $rebuild_redirect = (isset($parts['scheme']) ? "{$parts['scheme']}:" : '') . 
+            ((isset($parts['user']) || isset($parts['host'])) ? '//' : '') . 
+            (isset($parts['user']) ? "{$parts['user']}" : '') . 
+            (isset($parts['pass']) ? ":{$parts['pass']}" : '') . 
+            (isset($parts['user']) ? '@' : '') . 
+            (isset($parts['host']) ? "{$parts['host']}" : '') . 
+            (isset($parts['port']) ? ":{$parts['port']}" : '') . 
+            (isset($parts['path']) ? "{$parts['path']}" : '') . 
+            (isset($parts['query']) ? "?{$parts['query']}" : '') . 
+            (isset($parts['fragment']) ? "#{$parts['fragment']}" : '');
+
+            return redirect($rebuild_redirect);
+
+        }
+
+    	return redirect(   );
     }
 }
