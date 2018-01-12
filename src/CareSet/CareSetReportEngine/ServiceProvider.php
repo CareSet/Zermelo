eac72e765100e752bcec1d680d60a5a00012d408
Phu Nguyen <phu@careset.com>
2018-01-12 12:23:48 -0600

    change from jwt to report engine repoved unused files

--- src/CareSet/CareSetReportEngine/ServiceProvider.php
+++ src/CareSet/CareSetReportEngine/ServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace CareSet\CareSetJWTAuthClient;
+namespace CareSet\CareSetReportEngine;
 
 use Illuminate\Support\Facades\Storage;
 
@@ -16,13 +16,6 @@ Class ServiceProvider extends \Illuminate\Support\ServiceProvider
 	{
 
 		 $this->loadMigrationsFrom(__DIR__.'/migrations');
-
-		 $this->loadRoutesFrom(__DIR__.'/routes.php');
-
-		$router->middleware('careset', \CareSet\CareSetJWTAuthClient\Middleware\JWTClientMiddleware::class);
-
-		Storage::MakeDirectory(base_path('keys'));
-		//Storage::delete(base_path('app/User.php'));
 
 	    $this->publishes([
 	        __DIR__.'/config/caresetjwtclient.php' => config_path('caresetjwtclient.php'),
