1c0c628db2a3e27d20b79c4ce1eeceee81b0a1aa
Phu Nguyen <siphu@users.noreply.github.com>
2018-02-08 16:32:26 -0600

    Update web.routes.php

--- src/CareSet/CareSetReportEngine/web.routes.php
+++ src/CareSet/CareSetReportEngine/web.routes.php
@@ -2,29 +2,29 @@
 
 
 
-
     Route::get('/CareSetReport/{report_name}/{parameters?}', function($report_name,$parameters="")
     {
-
+    
         $Parameters = ($parameters=="")?[]:explode("/",$parameters);
         $Code = null;
-
+    
         if(count($Parameters) > 0)
         {
             $Code = array_shift($Parameters);
         }
-
+    
         try {
-            $report = "App\\CareSetReports\\{$report_name}";
+            $report = "App\\CareSetReports\\{$report_name}\\{$report_name}";
             $Report = new $report($Code,$Parameters);
         } catch(Exception $e)
         {
             abort(404);
         }
-
-        $Controller = new \App\CareSetReportEngine\Controllers\CareSetReportController;
+    
+        $Controller = new CareSet\CareSetReportEngine\Controllers\CaresetReportController;
         return $Controller->ReportDisplay($Report);
        
-
-
+    
+    
     })->where(['parameters' => '.*']);
+    
