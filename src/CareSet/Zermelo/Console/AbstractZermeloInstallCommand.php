5984732477f634f1684c1e08cdb2534582512152
Ken Chapple <ken.chapple@gmail.com>
2018-09-05 12:18:47 -0400

    Change to make zermelo for creating directory

--- src/CareSet/Zermelo/Console/AbstractZermeloInstallCommand.php
+++ src/CareSet/Zermelo/Console/AbstractZermeloInstallCommand.php
@@ -109,9 +109,40 @@ abstract class AbstractZermeloInstallCommand extends Command
 
     protected function exportAssets()
     {
-        File::copyDirectory(
-            $this->asset_path,
-            public_path('vendor/CareSet')
-        );
+        if ( !File::exists( public_path('vendor/CareSet' ) ) ) {
+            File::makeDirectory( public_path('vendor/CareSet' ) );
+        }
+
+        $new_files = File::allFiles( $this->asset_path );
+        $new_pathnames = [];
+        foreach ( $new_files as $new_file ) {
+            $relativePathname = $new_file->getRelativePathname();
+            $new_pathnames[]= $relativePathname;
+            if ( file_exists( public_path('vendor/CareSet').'/'.$relativePathname ) && ! $this->option('force')) {
+                if ( !$this->confirm("The [{$relativePathname}] asset already exists. Do you want to replace it?")) {
+                    continue;
+                }
+            }
+
+            // If we say yes, or we're running in "force" mode, copy asset
+            copy(
+                $this->asset_path.'/' .$relativePathname,
+                public_path('vendor/CareSet').'/'.$relativePathname
+            );
+        }
+
+//      This is not safe because we may delete an asset from another view package.
+//
+//        // If we say yes, or we're running in "force" mode, delete stale assets.
+//        $existing_files = File::allFiles( public_path('vendor/CareSet') );
+//        foreach ( $existing_files as $existing_file ) {
+//            $relativePathname = $existing_file->getRelativePathname();
+//            if ( !in_array( $relativePathname, $new_pathnames ) ) {
+//                if ( $this->option('force') ||
+//                    $this->confirm("The [{$relativePathname}] asset may no longer needed. Do you want to remove it?")) {
+//                    unlink( public_path('vendor/CareSet' ).'/'.$relativePathname );
+//                }
+//            }
+//        }
     }
 }
