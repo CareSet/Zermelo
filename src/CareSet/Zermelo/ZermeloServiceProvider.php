252809d0cf8644bc0f953bf961244566709083f9
Ken Chapple <ken.chapple@gmail.com>
2019-03-21 13:12:13 -0400

    making middleware customizable

--- src/CareSet/Zermelo/ZermeloServiceProvider.php
+++ src/CareSet/Zermelo/ZermeloServiceProvider.php
@@ -125,12 +125,19 @@ Class ZermeloServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     protected function routeConfiguration()
     {
+        $middleware = config('zermelo.MIDDLEWARE',[ 'api' ]);
+        $middlewareString = "";
+        foreach ( $middleware as $m ) {
+            $middlewareString .= "$m,";
+        }
+        $middlewareString = rtrim($middlewareString,",");
+
         return [
             'namespace' => 'CareSet\Zermelo\Http\Controllers',
             'domain' => config('zermelo.domain', null),
             'as' => 'zermelo.api.',
             'prefix' => config( 'zermelo.API_PREFIX' ),
-            'middleware' => 'api',
+            'middleware' => $middlewareString,
         ];
     }
 }
