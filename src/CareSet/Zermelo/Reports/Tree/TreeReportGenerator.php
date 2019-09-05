99cab75954351148b04d49ce45fc25de8fcf28cf
Fred Trotter <fred.trotter@gmail.com>
2019-09-05 03:43:13 -0500

    sorta works

--- src/CareSet/Zermelo/Reports/Tree/TreeReportGenerator.php
+++ src/CareSet/Zermelo/Reports/Tree/TreeReportGenerator.php
@@ -13,11 +13,15 @@ class TreeReportGenerator extends AbstractGenerator
     protected $cache = null;
     protected $report = null;
     protected $cache_db = '_zermelo_cache'; //TODO this should be set in config
+    protected $cache_table = 'will be changed';
 
     public function __construct(CachedTreeReport $cache)
     {
         $this->cache = $cache;
         $this->report = $cache->getReport();
+	$this->cache_db = '_zermelo_cache'; //more hard-coding.
+	$this->cache_table = $cache->getTableName();
+	
     }
 
     /**
@@ -33,182 +37,98 @@ class TreeReportGenerator extends AbstractGenerator
         $report_name = $this->report->getReportName();
 	$cache_db = '_zermelo_cache'; //should be coming from config TODO
 
-        //lets read in the node types
-
-        $node_types_sql = "
+	$select_sql = "
 SELECT 
-	id AS my_index,
-	node_type AS id,
-	node_type AS label,
-	0 AS is_img,
-	'' AS img_stub,
-	count_distinct_node AS type_count
-FROM $this->cache_db.{$this->cache->getNodeTypesTable()}	
-";
-        //lets load the node_types from the database...
-        $node_types = [];
-        $node_types_result = ZermeloDatabase::connection($this->cache->getConnectionName())->select(DB::raw($node_types_sql));
-        foreach ($node_types_result as $this_row) {
-
-            //handle the differeces between json and mysql/php here for is_img
-            if ($this_row->is_img) {
-                $is_img = false;
-            } else {
-                $is_img = $this_row->is_img;
-            }
-
-            $node_types[$this_row->my_index] = [
-                'id' => $this_row->id,
-                'label' => $this_row->label,
-                'is_img' => $is_img,
-                'img_stub' => $this_row->img_stub,
-                'type_count' => $this_row->type_count,
-            ];
-        }
-
-        //lets read in the link types
-
-        $link_types_sql = "
-SELECT 
-	id AS my_index,
-	link_type AS label,
-	count_distinct_link AS link_type_count
-FROM $this->cache_db.{$this->cache->getLinkTypesTable()}	
-";
-        //lets load the link_types from the database...
-        $link_types = [];
-        $link_types_result = ZermeloDatabase::connection($this->cache->getConnectionName())->select(DB::raw($link_types_sql));
-        foreach ($link_types_result as $this_row) {
-
-            $link_types[$this_row->my_index] = [
-                'id' => $this_row->label,
-                'label' => $this_row->label,
-                'link_type_count' => $this_row->link_type_count,
-            ];
-        }
-
-        //lets read in the link types
-
-        $group_sql = "
-SELECT 
-	id AS my_index,
-	group_name AS id,
-	group_name AS name,
-	count_distinct_node AS group_count
-FROM $this->cache_db.{$this->cache->getNodeGroupsTable()}	
+	root,
+	root_url,
+	branch,
+	branch_url,
+	leaf,
+	leaf_url
+FROM $this->cache_db.$this->cache_table
 ";
 
-        //lets load the link_types from the database...
-        $node_groups = [];
-        $node_groups_result = ZermeloDatabase::connection($this->cache->getConnectionName())->select(DB::raw($group_sql));
-        foreach ($node_groups_result as $this_row) {
+	$results = ZermeloDatabase::connection($this->cache->getConnectionName())->select(DB::raw($select_sql));
 
-            $node_groups[$this_row->my_index] = [
-                'id' => $this_row->id,
-                'name' => $this_row->name,
-                'group_count' => $this_row->group_count,
-            ];
-        }
+	$url_lookup = [];
+	$tree = [];
+	foreach($results as $this_row){
+		if(strlen($this_row->root_url) >  0){
+			$url_lookup[$this_row->root] = $this_row->root_url;
+		}
+		if(strlen($this_row->branch_url) > 0){
+			$url_lookup["$this_row->root$this_row->branch"] = $this_row->branch_url;
+		}
+		if(is_null($this_row->leaf)){
+			$tree[$this_row->root][$this_row->branch] = null;
+		}else{
+			$tree[$this_row->root][$this_row->branch][] = $this_row->leaf;
+			if(strlen($this_row->leaf_url) > 0){
+				$url_lookup["$this_row->root$this_row->branch$this_row->leaf"] = $this_row->leaf_url;
+			}
+		}	
+	}
 
-        //lets sort the nodes
-        $nodes_sql = "
-SELECT 
-	`node_name` AS name,
-	`node_latitude` AS latitude,
-	`node_json_url` AS json_url,
-	`node_longitude` AS longitude,
-	groups.id AS `group`,
-	node_size AS size,
-	node_img AS img,
-	types.id AS `type`,
-	`node_id` AS id,
-	0 AS weight_sum,
-	0 AS degree,
-	nodes.id AS my_index
-FROM $this->cache_db.{$this->cache->getNodesTable()} AS nodes
-LEFT JOIN $this->cache_db.{$this->cache->getNodeGroupsTable()} AS groups ON 
-	groups.group_name =
-    	node_group
-LEFT JOIN $this->cache_db.{$this->cache->getNodeTypesTable()} AS types ON 
-	types.node_type = 
-    	nodes.node_type 
-ORDER BY nodes.id ASC
-";
-        //lets load the link_types from the database...
-        $nodes = [];
-        $nodes_result = ZermeloDatabase::connection($this->cache->getConnectionName())->select(DB::raw($nodes_sql));
-        foreach ($nodes_result as $this_row) {
+	$return_me = [];
+//uncomment these for debugging..
+//	$return_me['tree'] =$tree;
+//	$return_me['url_lookup'] = $url_lookup;
+//here is basically what I am going for..
+/*
+	[
+		{
+			label: 'something',
+			url: 'a_url',
+			sub_tree: [
+				
+			]
+		}
+	]
+*/
+	$data = [];
+	foreach($tree as $root_node => $sub_node){
+		$tmp = [];
+		$tmp['label'] = $root_node;
+		if(isset($url_lookup[$root_node])){
+			$tmp['url'] = $url_lookup[$root_node];
+		}
+		
+		if(is_array($sub_node)){
+			$tmp['sub_tree'] = [];	
+			foreach($sub_node as $branch_node => $leaf_node){
+				$sub_tmp = [];
+				$sub_tmp['label'] = $branch_node;
+				if(isset($url_lookup["$root_node$branch_node"])){
+					$sub_tmp['url'] = $url_lookup["$root_node$branch_node"];
+				}
+				if(is_array($leaf_node)){
+					//this is the typical three-layer case..
+					$sub_tmp['sub_tree'] = [];
+					foreach($leaf_node as $leaf_label){
+						$sub_sub_tmp = [];
+						$sub_sub_tmp['label'] = $leaf_label;
+						if(isset($url_lookup["$root_node$branch_node$leaf_label"])){
+							$sub_sub_tmp['url'] = $url_lookup["$root_node$branch_node$leaf_label"];
+						}
+						$sub_tmp['sub_tree'][] = $sub_sub_tmp;
+					}
+				}else{
+					//this is branch=leaf
+					//do nothing..
+				}
+				$tmp['sub_tree'][] = $sub_tmp;
+			}
+		}else{
+			//this is root=leaf..
+			//do nothing for now...
+		}
+		$data[] = $tmp;
+	}
 
-            if (is_null($this_row->img)) {
-                $img = false;
-            } else {
-                $img = $this_row->img;
-            }
+	$return_me['data'] = $data;
 
-            //we would this version result in an object instead of an array?? confusing
-//		$nodes[$this_row->my_index] = [
-            $nodes[(int) $this_row->my_index] = [
-                'name' => $this_row->name,
-                'short_name' => substr($this_row->name, 0, 20),
-                'longitude' => $this_row->longitude,
-                'latitiude' => $this_row->latitude,
-		'json_url' => $this_row->json_url,
-                'group' => (int)$this_row->group,
-                'size' => (int)$this_row->size,
-                'img' => $img,
-                'type' => (int)$this_row->type,
-                'id' => $this_row->id,
-                'weight_sum' => (int)$this_row->weight_sum,
-                'degree' => (int)$this_row->degree,
-                'my_index' => (int)$this_row->my_index,
-            ];
-        }
+	return($return_me);
 
-	//nodes are built, but we want to make sure that it turns into an array in the json rather than object..
-	$nodes = array_values($nodes); //should return  a zero indexed array. not sure why this converts it to an array.. but it does....
-
-        // Retrieve the links from the DB
-        $links_sql = "SELECT * FROM $this->cache_db.`{$this->cache->getLinksTable()}`";
-        $links = [];
-        $links_result = ZermeloDatabase::connection($this->cache->getConnectionName())->select(DB::raw($links_sql));
-        foreach ($links_result as $this_row) {
-
-            $links[] = [
-                'source' => $this_row->source,
-                'target' => $this_row->target,
-                'weight' => $this_row->weight,
-                'link_type' => $this_row->link_type,
-            ];
-        }
-
-        //lets export the summary data on the graph
-        $summary_sql = "
-            SELECT 
-                summary_key,
-                summary_value
-            FROM $this->cache_db.{$this->cache->getSummaryTable()}";
-
-        $summary = [];
-        $summary_result = ZermeloDatabase::connection($this->cache->getConnectionName())->select(DB::raw($summary_sql));
-        foreach ($summary_result as $this_row) {
-            $summary[][$this_row->summary_key] = $this_row->summary_value;
-        }
-
-        //now we put it all together to return the results...
-        return [
-            'careset_name' => 'For backwards compatibility',
-            'careset_code' => '1112223334',
-            'Report_Name' => $report_name,
-            'Report_Description' => $report_description,
-            'Report_Key' => $this->cache->getKey(),
-            'summary' => $summary,
-            'config' => [], //not implemented..
-            'groups' => $node_groups,
-            'types' => $node_types,
-            'link_types' => $link_types,
-            'nodes' => $nodes,
-            'links' => $links,
-        ];
     }
 
 
