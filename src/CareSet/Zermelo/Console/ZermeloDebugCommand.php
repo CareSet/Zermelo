2a47814923a638f3f50fd5980f4610f3a541c07b
Fred Trotter <fred.trotter@gmail.com>
2019-10-13 20:34:03 -0500

    better error messages

--- src/CareSet/Zermelo/Console/ZermeloDebugCommand.php
+++ src/CareSet/Zermelo/Console/ZermeloDebugCommand.php
@@ -34,21 +34,32 @@ class ZermeloDebugCommand extends Command
      */
     public function handle()
     {
+	$db_name = DB::connection()->getDatabaseName();
+
         // Check encoding and warn if there's non-matching encodings
         $encodingResult = DB::connection()->select("
             SHOW VARIABLES 
-            WHERE ( Variable_name LIKE 'character\_set\_%' OR Variable_name LIKE 'collation%'  ) AND 
-            Variable_name != 'character_set_filesystem' AND Variable_name != 'character_set_system'");
+            WHERE 
+			( Variable_name LIKE 'character\_set\_%' OR Variable_name LIKE 'collation%'  ) 
+		AND 
+            		Variable_name != 'character_set_filesystem' 
+		AND 
+			Variable_name != 'character_set_system'
+		AND 
+			Value != 'utf8mb4_unicode_ci'
+");
 
         if (count($encodingResult) > 0) {
-            $this->comment("You have mismatched character sets which may cause issues with displaying your data");
-            $headers = ['Variable_name', 'Value'];
+            $this->comment("Your database connection (through $db_name)  has character sets which may cause issues with displaying your data");
+            $headers = ['Database setting', 'Value'];
             $array = [];
             foreach ($encodingResult as $value) {
                 $row = [$value->Variable_name, $value->Value];
                 $array[]= $row;
             }
             $this->table($headers, $array);
+		$this->comment("Please consider just switching everything to utf8mb4_unicode_ci");
+		$this->comment("Because https://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci/766996#766996");	
         }
     }
 }
