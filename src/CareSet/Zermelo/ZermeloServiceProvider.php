56f8a1ae4825cbf8792e003a8c9846733f2791b8
Fred Trotter <fred.trotter@gmail.com>
2019-10-13 21:21:16 -0500

    better GRANT commands

--- src/CareSet/Zermelo/ZermeloServiceProvider.php
+++ src/CareSet/Zermelo/ZermeloServiceProvider.php
@@ -86,8 +86,20 @@ Class ZermeloServiceProvider extends \Illuminate\Support\ServiceProvider
 
                 $username = config( "database.connections.$default.username" );
                 $message .= "You are trying to connect with mysql user `$username`, you may have to run the following commands:\n";
-                $message .= "GRANT ALL on _zermelo_cache TO $username@localhost\n";
-                $message .= "GRANT ALL on _zermelo_config TO $username@localhost\n";
+		$message .= "
+GRANT 
+      SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, LOCK TABLES 
+ON `\_zermelo\_cache`.* 
+TO '$username'@'%' 
+";
+		$message .= "
+GRANT 
+   SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, LOCK TABLES 
+ON `\_zermelo\_config`.* 
+TO '$username'@'%' 
+;
+";
+
             }
             throw new \Exception($message);
         }
