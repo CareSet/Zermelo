1bd5fabe928301159a51e94b53812f1f12f8b943
Fred Trotter <fred.trotter@gmail.com>
2021-03-28 00:08:45 +0000

    change how the zebra works

--- views/zermelo/layouts/card_layout.blade.php
+++ views/zermelo/layouts/card_layout.blade.php
@@ -6,7 +6,7 @@
 
 <link rel="stylesheet" type="text/css" href='{{ $bootstrap_css_location }}'/>
 <link rel="stylesheet" type="text/css" href='{{ asset("vendor/CareSet/zermelobladecard/datatables/datatables.min.css") }}'/>
-<link href='{{ asset("vendor/CareSet/zermelo/core/font-awesome/css/all.min.css") }}' rel="stylesheet" />
+<link href='{{ asset("vendor/CareSet/zermelobladecard/fontawesome-free-5.10.2-web/css/all.css") }}' rel="stylesheet" />
 <style type="text/css">
     h1.card-title {
         display: inline-block;
@@ -20,10 +20,20 @@
         height: min-content;
         vertical-align: middle;
     }
+
+    .alternate_row {
+        background-color: whitesmoke;
+        border-radius: 10px;
+        padding-top: 5px;
+
+    }
+    
+
 </style>
 </head>
 <body>
 
+@include('Zermelo::menu')
 
 @include('Zermelo::card')
 
