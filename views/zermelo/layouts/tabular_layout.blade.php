321a5009dccd931f08538918285f9e235d7ea9ac
Fred Trotter <fred.trotter@gmail.com>
2021-04-25 20:48:57 +0000

    inline styles to help the layout to be more dense

--- views/zermelo/layouts/tabular_layout.blade.php
+++ views/zermelo/layouts/tabular_layout.blade.php
@@ -9,6 +9,23 @@
     <link rel="stylesheet" type="text/css" href='{{ asset("vendor/CareSet/zermelo/core/css/caresetreportengine.report.css") }}' />
     <link rel="stylesheet" type="text/css" href='{{ asset("vendor/CareSet/zermelo/zermelobladetabular/datatables/datatables.min.css") }}' />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+<!-- inline styles foce the headings on the table to be more dense for smaller columns --> 
+<style type="text/css">
+.yadcf-filter {
+    width: 60px !important;
+    max-width: 60px !important;
+}
+
+.yadcf-filter-wrapper {
+    display: block !important;
+}
+
+
+</style>
+
+
 </head>
 <body>
 
